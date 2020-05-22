9099ec237f2802d909f0127b4617f15fe2f42ed7
Cat <39655575+KittenMeow@users.noreply.github.com>
2020-05-22 10:01:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 print("Hello, world!")
 age = 10
 print("You are ", age, " years old!")
+if age > 10:
+	print("You are older than 10!")
+elif age < 10:
+	print("You are younger than 10!")
+else:
+	print("You are 10!")
+	
+name = input("Enter your name.")
+
+if age >= 10 && name = "Charles":
+	print("You are 10 or older and your name is Charles.")
+elif age >= 10 || name = "Charles":
+	print("Either you are 10 or older or you are Charles.")
+elif name != "Charles":
+	print("You are not Charles.")
+else:
+	print(f'You are {age} years old and your name is {name}.')
